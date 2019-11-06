1bb5eccf0e27b6cc221e95fd97da3479ac3cbccd
Diego Pastoriza <dpastoriza89@gmail.com>
2019-11-06 20:30:28 -0300

    pusheada

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,9 +1,27 @@
+const argv = require('yargs')
+      .command('listar','Imprime en consola la tabla de multiplicar',{
+        base:{
+            demand:true,// obligatorio
+            alias:'b'
+        }, 
+        limite:{
+            default:10,
+            alias:'l'
+        }
+      }).argv;
 const {crearArchivo} = require('./multiplicar/multiplicar');
 
 
-let tabla = 7;
 
-crearArchivo('hola').then(archivo => {
+
+let argv2 = process.argv;
+//node app --base=6
+let parametro = argv[2];
+
+
+console.log(argv.base);
+
+crearArchivo(argv.base).then(archivo => {
     console.log(`Archivo creado ${archivo}`);
     
 }).catch( error => {
